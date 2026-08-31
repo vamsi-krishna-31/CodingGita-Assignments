4ea84efaab49751e875876215a20d8b0b34d5808
Vamsi Krishna <vamsii.krishna.cg@gmail.com>
2026-08-31 18:01:44 +0530

    Create solutions.py

---
+++ Semister-1/PYTHON assignments/Assignment 1 solutions/Topic 2- Print( )/solutions.py
@@ -0,0 +1,155 @@
+'''
+#  Solution_13
+
+name="Rahul Raj"
+age=18
+course=B.Tech
+city="Patna"
+print("My name is",name,". I am",age,"years old. I am studying",course,"and I live in Patna.")
+'''
+
+'''
+#  SOLUTION_15
+
+name="Priya Sharma"
+age=19
+gender=Female
+city="Jaipur"
+print("My name is",name,". I am",age,"years old. I am",gender,"and I live in Jaipur")
+'''
+
+'''
+# SOLUTION_14
+
+# Variable declarations
+student_name = "Aman Kumar"
+roll_number = 101
+branch = "Computer Science"
+semester = 1
+
+# Formatting and displaying the output
+print(f"My name is {student_name}. My roll number is {roll_number}. I am studying {branch} in semester {semester}.")
+'''
+
+'''
+# SOLUTION_16
+
+brand = "Samsung"
+model = "Galaxy A55"
+ram = "8 GB"
+storage = "256 GB"
+
+print(f"I have a {brand} {model} with {ram} and {storage})
+'''
+
+'''
+# SOLUTION_17
+
+# Variable declarations
+employee_name = "Rohit Kumar"
+job_role = "Software Developer"
+company = "ABC Technologies"
+experience = "2 years"
+
+# Formatting and displaying the output
+print(f"My name is {employee_name}. I work as a {job_role} at {company} and I have {experience} of experience.")
+'''
+
+'''
+# SOLUTION_18
+
+# Variable declarations
+college_name = "XYZ Institute of Technology"
+course = "B.Tech"
+branch = "Information Technology"
+city = "Ahmedabad"
+
+# Formatting and displaying the output
+print(f"I am studying {course} in {branch} at {college_name} in {city}.")
+'''
+
+'''
+# SOLUTION_19
+
+# Variable declarations
+product_name = "Laptop"
+brand = "HP"
+price = 55000
+quantity = 1
+
+# Formatting and displaying the output
+print(f"I bought {quantity} {brand} {product_name} for {price} rupees.")
+'''
+
+'''
+# SOLUTION_20
+
+# Variable declarations
+my_name = "Rahul Raj"
+father_name = "Rajesh Kumar"
+mother_name = "Sunita Kumar"
+city = "Patna"
+
+# Formatting and displaying the output
+print(f"My name is {my_name}. My father's name is {father_name}. My mother's name is {mother_name}. I live in {city}")
+'''
+
+'''
+# SOLUTION_21
+
+# Variable declarations
+student_name = "Neha Sharma"
+subject = "Python Programming"
+marks = 88
+grade = "A"
+percentage = 88.0
+
+# Formatting and displaying the output
+print(f"{student_name} scored {marks} marks in {subject}. Her grade is {grade} and her percentage is {percentage}.")
+'''
+
+'''
+# SOLUTION_22
+
+# Variable declarations
+brand = "HP"
+model = "Pavilion 15"
+processor = "Intel Core i5"
+ram = "16 GB"
+storage = "512 GB SSD"
+price = 65000
+
+# Formatting and displaying the output
+print(f"I have an {brand} {model} with {processor}, {ram} RAM, {storage}, and it costs {price} rupees.")
+'''
+
+'''
+# SOLUTION_23
+
+# Variable declarations
+movie_name = "3 Idiots"
+genre = "Comedy Drama"
+rating = 8.4
+language = "Hindi"
+release_year = 2009
+
+# Formatting and displaying the output
+print(f"{movie_name} is a {genre} movie in {language}. It was released in {release_year} and has a rating of {rating}.")
+'''
+
+'''
+# SOLUTION_24
+
+# Variable declarations
+student_name = "Arjun Patel"
+age = 19
+college = "ABC Institute of Technology"
+branch = "Computer Science"
+semester = 1
+city = "Ahmedabad"
+percentage = 87.5
+favorite_language = "Python"
+
+# Formatting and displaying the output
+print(f"My name is {student_name}. I am {age} years old and I am studying {branch} in semester {semester} at {college} in {city}. I scored {percentage} percent and my favorite programming language is {favorite_language}.")
+'''
