1b6386854d5623d6312384b6e7f2e589acb7395d
Vamsi Krishna <vamsii.krishna.cg@gmail.com>
2026-08-31 18:00:37 +0530

    Create solutions.py

---
+++ Semister-1/PYTHON assignments/Assignment 1 solutions/Topic 1- Variables and Variable Naming/solutions.py
@@ -0,0 +1,106 @@
+''' 
+# SOLUTION_1
+
+name = "Raju"
+print("Name :",name) 
+'''
+
+'''
+# SOLUTION_2
+
+student_name="Aman"
+print("Student Name :", student_name)
+'''
+
+'''
+# SOLUTION_3
+
+student_name="Rahul
+print("Student Name :",student_name)
+'''
+
+'''
+# SOLUTION_4
+
+student_name ="Ravi"
+print("Student Name :", student_name)
+'''
+
+'''
+# SOLUTION_5
+
+student_name = "Rohan"
+print("Student Name:", student_name )
+'''
+
+'''
+# SOLUTION_6
+
+student_name = "Neha"
+student_age = 19
+
+print("Name:", student_name)
+print("Age:", student_age)
+'''
+
+'''
+# SOLUTION_7
+
+student_name_1 = "Karan"
+student_name_2 = "Raj"
+
+print("Student 1:", student_name)
+print("Student 2:", student_name)
+
+'''
+
+'''
+# SOLUTION_8
+
+student_name = "Priya"
+student_age = 19
+
+print("Name:", student_name)
+print("Age:", student_age)
+'''
+
+'''
+# SOLUTION_9
+
+name = "Amit"
+student_age = 18
+
+print("Name:", name)
+print("Age:", student_age)
+'''
+
+'''
+# SOLUTION_10
+
+college_name = "ABC College"
+student_name = "Meera"
+
+print("College:", college_name)
+print("Student:", student_name)
+'''
+
+'''
+# SOLUTION_11
+
+first_name = "Raj"
+last_name = "Patel"
+
+print("Full Name:", first_name, last_name)
+'''
+
+'''
+# SOLUTION_12
+
+first_name = "Raj"
+last_name = "Patel"
+
+print("Full Name:", first_name, last_name)
+
+'''
+
+print("Thank You")
