b6716dd6b3f37eb6ccd8e2af285607342b6393e2
Vamsi Krishna <vamsii.krishna.cg@gmail.com>
2026-08-31 18:03:15 +0530

    Create assignments.py

---
+++ Semister-1/PYTHON assignments/Assignment 1 solutions/Topic 3- Data Types and type( )/assignments.py
@@ -0,0 +1,128 @@
+'''
+# SOLUTION_21
+
+name = "Riya"
+age = 20
+marks = 85.5
+
+print("Name:", name)
+print("Age:", age)
+print("Marks:", marks)
+
+print("Name Type:", type(name))
+print("Age Type:", type(age))
+print("Marks Type:", type(marks))
+'''
+
+'''
+# SOLUTION_22
+
+name = "Riya"
+age = 20
+marks = 85.5
+
+print("Name:", name)
+print("Age:", age)
+print("Marks:", marks)
+
+print("Name Type:", type(name))
+print("Age Type:", type(age))
+print("Marks Type:", type(marks))
+'''
+
+
+'''
+# SOLUTION_23
+
+age = "18"
+marks = 85.5
+
+print("Age:", age)
+print("Marks:", marks)
+print("Age Type:", type(age))
+print("Marks Type:", type(marks))
+'''
+
+'''
+# SOLUTION_24
+
+age = 18
+marks = "85.5"
+
+print("Age:", age)
+print("Marks:", marks)
+
+print("Age Type:", type(age))
+print("Marks Type:", type(marks))
+'''
+
+'''
+# SOLUTION_25
+
+name = "Ravi"
+age = 18
+marks = 90.5
+passed = True
+
+print("Name:", name)
+print("Age:", age)
+print("Marks:", marks)
+print("Passed:", passed)
+
+print(type(name))
+print(type(age))
+print(type(marks))
+print(type(passed))
+'''
+
+
+'''
+# SOLUTION_25
+
+name = "Ravi"
+age = 18
+marks = 90.5
+passed = True
+
+print("Name:", name)
+print("Age:", Age)
+print("Marks:", marks)
+print("Passed:", passed)
+
+print(type(name))
+print(type(age))
+print(type(mark))
+print(type(passed))
+'''
+
+'''
+# SOLUTION_26
+
+student_name = "Karan"
+student_age = 18
+student_marks = 87.5
+
+print("Student:", student_name)
+print("Age:", student_age)
+print("Marks:", student_marks)
+
+print("Student Type:", type(student_name))
+print("Age Type:", type(student_age))
+print("Marks Type:", type(student_marks))
+'''
+
+'''
+# SOLUTION_27
+
+student_name = "Karan"
+student_age = 18
+student_marks = 87.5
+
+print("Student:", student_name)
+print("Age:", student_age)
+print("Marks:", student_marks)
+
+print("Student Type:", type(student_Name))
+print("Age Type:", type(student_age))
+print("Marks Type:", type(student_marks))
+'''
