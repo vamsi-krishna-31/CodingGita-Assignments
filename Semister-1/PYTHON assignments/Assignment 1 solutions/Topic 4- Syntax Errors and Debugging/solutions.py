ecc83273c7e1dd49cdbfdcba4cbae01d0b958e3a
Vamsi Krishna <vamsii.krishna.cg@gmail.com>
2026-08-31 18:04:24 +0530

    Create solutions.py

---
+++ Semister-1/PYTHON assignments/Assignment 1 solutions/Topic 4- Syntax Errors and Debugging/solutions.py
@@ -0,0 +1,203 @@
+'''
+# SOLUTION_29
+
+name = "Riya"
+age = 20
+
+print("Name:", name)
+print("Age:", age)
+'''
+
+'''
+# SOLUTION_30
+
+name = "Aman"
+age = 18
+
+print("Name:", name)
+print("Age:", age)
+'''
+
+
+'''
+# SOLUTION_31
+
+student_name = "Rohan"
+student_age = 19
+student_city = "Ahmedabad"
+
+print("Name:", student_name)
+print("Age:", student_age)
+print("City:", student_city)
+'''
+
+'''
+# SOLUTION_32
+
+student_name = "Neha"
+student_age = 20
+student_city = "Vadodara"
+
+print("Name:", student_name)
+print("Age:", student_age)
+print("City:", student_city)
+
+print("Name Type:", type(student_name))
+print("Age Type:", type(student_age))
+print("City Type:", type(student_city))
+'''
+
+'''
+# SOLUTION_33
+
+student_name = "Amit"
+student_age = 18
+student_college = "XYZ College"
+
+print("Name:", student_name)
+print("Age:", student_age)
+print("College:", student_college)
+
+print("Name Type:", type(student_name))
+print("Age Type:", type(student_age))
+'''
+
+'''
+# SOLUTION_34
+
+student_name = "Meera"
+student_age = 20
+student_college = "ABC College"
+
+print("Name:", student_name)
+print("Age:", student_age)
+print("College:", student_college)
+
+print("Name Type:", type(student_name))
+print("Age Type:", type(student_age))
+print("College Type:", type(student_college))
+'''
+
+'''
+# SOLUTION_35
+
+student_name = "Raju"
+student_age = 18
+student_marks = 92.5
+is_passed = True
+
+print("Name:", student_name)
+print("Age:", student_age)
+print("Marks:", student_marks)
+print("Passed:", is_passed)
+
+print("Name Type:", type(student_name))
+print("Age Type:", type(student_age))
+print("Marks Type:", type(student_marks))
+print("Passed Type:", type(is_passed))
+'''
+
+'''
+# SOLUTION_36
+
+student name = "Raju"
+student_age = 18
+student_marks = 92.5
+is_passed = True
+
+print("Name:", student_name)
+print("Age:", student_age)
+print("Marks:", student_marks)
+print("Passed:", is_passed)
+'''
+
+'''
+# SOLUTION_37
+
+name = "Amit"
+age = 18
+marks = 91.5
+
+print("Name:", name)
+print("Age:", age)
+print("Marks:", marks)
+
+print("Name Type:", type(name))
+print("Age Type:", type(age))
+print("Marks Type:", type(marks))
+'''
+
+'''
+# SOLUTION_38
+
+college_name = "ABC College"
+student_name = "Riya"
+student_age = 19
+
+print("College:", college_name)
+print("Student:", student_name)
+print("Age:", student_age)
+print("College Type:", type(college_name))
+'''
+
+'''
+# SOLUTION_39
+
+class = "B.Tech"
+student_name = "Ravi"
+
+print("Class:", class)
+print("Student:", student_name)
+'''
+
+'''
+# SOLUTION_40
+
+student_name = "Ravi"
+student_age = 18
+student_marks = 89.5
+
+print("Name:", student_name)
+print("Age:", student_age)
+print("Marks:", student_marks)
+
+print("Name Type:", type(student_name))
+print("Age Type:", type(student_age))
+print("Marks Type:", type(student_marks))
+'''
+
+'''
+# SOLUTION_41
+
+student_name = "Ravi"
+student_age = 18
+student_marks = 89.5
+
+print("Name:", student_name)
+print("Age:", student_age)
+print("Marks:", student_marks)
+
+print("Name Type:", type(student_name))
+print("Age Type:", type(student_age))
+print("Marks Type:", type(student_marks))
+'''
+
+'''
+# SOLUTION_42
+
+name = "Karan"
+age = "18"
+marks = 87.5
+college_name = "ABC College"
+
+print("Name:", name)
+print("Age:", age)
+print("Marks:", marks)
+print("College:", college_name)
+
+print("Name Type:", type(name))
+print("Age Type:", type(age))
+print("Marks Type:", type(marks))
+print("College Type:", type(college_name))
+'''
+Thank you
