f0556dc3b599bd460a5971d4e5144b37955d8cef
Vamsi Krishna <vamsii.krishna.cg@gmail.com>
2026-09-22 10:27:39 +0530

    Create solutions.py

---
+++ Semister-1/PYTHON assignments/Assignment 3 solutions/Topic-2: Assignment Operators/solutions.py
@@ -0,0 +1,31 @@
+# Topic-2 — Assignment Operators
+# Q5 to Q6
+
+
+# Q5 — Trace the Value
+x = 20
+
+x += 10
+print("After += 10:", x)   # 30
+
+x -= 5
+print("After -= 5:", x)    # 25
+
+x *= 2
+print("After *= 2:", x)    # 50
+
+x //= 5
+print("After //= 5:", x)   # 10
+
+print("Final Value:", x)   # 10
+
+
+# Q6 — Assignment Operator Practice
+marks = 50
+
+marks += 10
+marks -= 5
+marks *= 2
+
+print("Final Marks:", marks)
+# Output: Final Marks: 110
