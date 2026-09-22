c695e80947e183d1254821d5f340c9e0e03d43ca
Vamsi Krishna <vamsii.krishna.cg@gmail.com>
2026-09-22 11:46:56 +0530

    Create solutions.py

---
+++ Semister-1/PYTHON assignments/Assignment 3 solutions/Topic-8 :Escape Sequences/solutions.py
@@ -0,0 +1,33 @@
+# Topic-8 — Escape Sequences
+
+# Q42. New Line
+print("Hello\nWorld")
+
+
+# Q43. Tab
+print("Name:\tRahul")
+print("Age:\t20")
+print("City:\tAhmedabad")
+
+
+# Q44. Backslash
+print("C:\\Python\\Programs")
+
+
+# Q45. Single Quote
+print("It\'s Python")
+
+
+# Q46. Double Quote
+print("He said \"Hello\"")
+
+
+# Q47. Predict the Output
+print("Python\nProgramming")
+
+
+# Q48. Combined Escape Sequences
+print("Student Details\n")
+print("Name:\tRahul")
+print("Age:\t20")
+print("Course:\tB.Tech")
