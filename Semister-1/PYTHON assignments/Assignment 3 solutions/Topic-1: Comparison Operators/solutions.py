be78ac596c75583fb25ccb9f4e55fe14c8ffaf91
Vamsi Krishna <vamsii.krishna.cg@gmail.com>
2026-09-22 10:25:30 +0530

    Create solutions.py

---
+++ Semister-1/PYTHON assignments/Assignment 3 solutions/Topic-1: Comparison Operators/solutions.py
@@ -0,0 +1,73 @@
+# Topic-1 — Comparison Operators
+# Q1 to Q4
+
+
+# Q1 — Predict the Output
+a = 15
+b = 20
+
+print(a < b)
+print(a > b)
+print(a == b)
+print(a != b)
+print(a <= b)
+print(a >= b)
+
+# Output:
+# True
+# False
+# False
+# True
+# True
+# False
+
+
+# Q2 — Compare Expressions
+x = 10
+y = 10
+
+print(x == y)
+print(x != y)
+print(x < y)
+print(x <= y)
+print(x >= y)
+
+# Output:
+# True
+# False
+# False
+# True
+# True
+
+# == is used for comparison.
+# = is used for assignment.
+
+
+# Q3 — Comparison with Arithmetic
+a = 10
+b = 5
+
+print(a + b == 15)
+print(a * b > 40)
+print(a - b != 5)
+print(a // b == 2)
+
+# Output:
+# True
+# True
+# False
+# True
+
+
+# Q4 — String Comparison
+print("Python" == "Python")
+print("Python" == "python")
+print("Hello" != "hello")
+
+# Output:
+# True
+# False
+# True
+
+# String comparison is case-sensitive.
+# "Python" and "python" are considered different strings.
